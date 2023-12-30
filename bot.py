71fa69e325a8533fb6b89690b5e3a43878f567b9
Dmiriy Yampolkiy <yampolskiydv@gmail.com>
2023-12-30 21:20:10 +0400

    POC is working

--- bot.py
+++ bot.py
@@ -1,43 +1,60 @@
 import asyncio
+import os
+from io import BytesIO
 from typing import Final
 
-from loguru import logger
-import os
-
-from telegram import Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, Dispatcher
 from fire import Fire
+from loguru import logger
+from telegram import Bot, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, Application, PicklePersistence
+
+from gpt_api import chat_gpt_description
 
 TG_BOT_TOKEN: Final[str] = os.getenv("TG_BOT_TOKEN")
 UPDATE_QUEUE: Final[asyncio.Queue] = asyncio.Queue()
+# JOB_QUEUE: Final[asyncio.Queue] = asyncio.Queue()
 BOT_INSTANCE: Final[Bot] = Bot(TG_BOT_TOKEN)
+PERSISTENCE: Final[PicklePersistence] = PicklePersistence(filepath="bot_persistence.pkl")
 UPDATER: Final[Updater] = Updater(bot=BOT_INSTANCE, update_queue=UPDATE_QUEUE)
-DISPATCHER: Final[Dispatcher] = Dispatcher(bot=BOT_INSTANCE, update_queue=UPDATE_QUEUE)
+APPLICATION: Final[Application] = (
+    Application
+    .builder()
+    .updater(UPDATER)
+    .build()
+)
+
 
 async def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi! Send me a picture and I will save it.')
+    await update.message.reply_text('Hi! Send me a picture and I will save it.')
 
-async def save_photo(update, context):
+
+async def process_photo(
+        update: Update,
+        context,
+):
     """Save photo sent by the user."""
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('pictures/photo.jpg')
-    update.message.reply_text('Photo saved!')
+    photo_file = await update.message.photo[-1].get_file()
+    io = BytesIO()
+    await photo_file.download_to_memory(io)
+    text = await chat_gpt_description(io)
+    await update.message.reply_text(text)
+
 
 async def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning(f'Update {update} caused error {context.error}')
 
 
-async def main():
+def main():
     # Register handlers
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(filters.PHOTO, save_photo))
-    dp.add_error_handler(error)
+    APPLICATION.add_handler(CommandHandler("start", start))
+    APPLICATION.add_handler(MessageHandler(filters.PHOTO, process_photo))
+    APPLICATION.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    updater.idle()
+    APPLICATION.run_polling(allowed_updates=Update.ALL_TYPES)
+
 
 if __name__ == '__main__':
     Fire(main)
--- gpt_api.py
+++ gpt_api.py
@@ -1,53 +1,48 @@
 import base64
 import os
-from pathlib import Path
+from io import BytesIO
+from typing import Final
 
-from openai import OpenAI
+from openai import AsyncOpenAI
 
-API_KEY = os.getenv("OPENAI_API_KEY")
+API_KEY: Final[str] = os.getenv("OPENAI_API_KEY")
+OPEN_AI_CLIENT: Final[AsyncOpenAI] = AsyncOpenAI(api_key=API_KEY)
 
-print("API_KEY", API_KEY)
-
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=API_KEY,
-)
 
 # Function to encode the image
-def encode_image(image_path: os.PathLike) -> str:
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+def encode_image(data: BytesIO) -> str:
+    return base64.b64encode(data.getbuffer()).decode('utf-8')
 
 
-image_path = "image.jpg"
+async def chat_gpt_description(data: BytesIO) -> str:
+    # Getting the base64 string
+    base64_image = encode_image(data)
 
-# Getting the base64 string
-base64_image = encode_image(Path(image_path))
+    response = await OPEN_AI_CLIENT.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What’s in this image? "
+                                "Produce only semantic search terms which can be used to look for this image"
+                                "Mention the objects, people, places, actions, activities, "
+                                "colors and text in the image."
+                                "Recognise all the text to be able to search for it."
+                                "Create terms in english and then in russian language."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    },
+                ],
+            },
+        ],
+        max_tokens=300,
+    )
 
-reqsponse = client.chat.completions.create(
-    model="gpt-4-vision-preview",
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "What’s in this image? "
-                            "Produce only semantic search terms which can be used to look for this image"
-                            "Mention the objects, people, places, actions, activities, colors and text in the image."
-                            "Recognise all the text to be able to search for it."
-                            "Create terms in english and then in russian language."
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}"
-                    }
-                },
-            ],
-        },
-    ],
-    max_tokens=300,
-)
-
-print(reqsponse)
+    return str(response.choices[0].message.content)
